23143e45ec1286cacdf80e3acb43427dd3afc886
Mathieu Mangeot <mangeot@MatMac.local>
2016-12-10 09:02:16 +0100

    Mise à jour de la table de manière asynchrone

--- shishito/ViewController.swift
+++ shishito/ViewController.swift
@@ -11,6 +11,7 @@ import Fuzi
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, XMLParserDelegate {
 // https://grokswift.com/simple-rest-with-swift/
+//https://medium.com/@ronm333/a-simple-table-view-example-in-swift-cbf9a405f975#.8viy2fgos
 
     var entries = NSMutableArray()
     var entry = NSMutableDictionary()
@@ -25,6 +26,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func buttonPressed(_ sender: Any) {
         print("You clicked the button")
+        self.callURL()
+    }
+    
+    func callURL () {
         var queryWord = searchField.text
         queryWord = queryWord?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
         let queryUrl: String = "https://jibiki.imag.fr/jibiki/api/Cesselin/jpn/cdm-writing/" + queryWord! + "/entries/?strategy=equal"
@@ -55,11 +60,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             do {
                 let document = try XMLDocument(data: responseData)
                 document.definePrefix("d", defaultNamespace: "http://www-clips.imag.fr/geta/services/dml")
+                self.entries = []
                 for article in document.xpath("/d:entry-list/d:entry/volume/d:contribution/d:data/article") {
                     print("id:"+article["id"]!)
                     self.entry = [:]
                     for (index, vedette) in article.xpath("./forme/vedette/vedette-jpn/text()").enumerated() {
-                        self.entry["vedette-jpn"] = vedette.stringValue
+                         self.entry["vedette-jpn"] = vedette.stringValue
                         if index == 1 {
                             break
                         }
@@ -81,23 +87,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print("vedh: " + (self.entry["vedette-hiragana"] as! String))
                     self.entries.add(self.entry)
                }
+                DispatchQueue.main.async { [unowned self] in
+                    self.myTableView.reloadData()
+                }
                 
-            self.myTableView.reloadData()
             } catch  {
                 print("error trying to convert data to JSON")
                 return
             }
         }
         task.resume()
-     //   self.myTableView.reloadData()
-
-        //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshViews) name:UIApplicationWillEnterForegroundNotification object:nil];
-
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    //    cars = ["BMW","Audi", "Volkswagen"]
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello world")
     }
@@ -115,6 +118,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return entries.count
     }
     
+    // ne marche pas
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell,
+                   forRowAt indexPath: IndexPath) {
+        
+        if (indexPath.row % 2 == 0)
+        {
+            cell.backgroundColor = UIColor.gray
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.white
+        }
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! TableViewCell
         let entry = self.entries[indexPath.row] as! NSMutableDictionary
@@ -124,7 +141,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         return cell
     }
-
 
 }
 
